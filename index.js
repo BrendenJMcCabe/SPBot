facbac7a816557e464a2fe32c30dcc56f7f9a3a6
Brenden <brendenjmccabe@gmail.com>
2023-10-16 20:15:54 -0500

    V1 of nickname redeem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ModalBuilder } = require('discord.js');
 const { token } = require('./config.json');
 let pointGen = { on: false };
 
